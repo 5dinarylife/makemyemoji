755d1d7f1ee483092659ff58bf3d04607f72b6eb
5dinarylife <5dinarylife@gmail.com>
2024-10-09 13:22:36 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "open-emoji-window") {
-    chrome.action.openPopup(); // 단축키로 이모지 팝업 열기
-  }
+chrome.commands.onCommand.addListener((command) => {
+    if (command === "open-emoji-selector") {
+        chrome.action.openPopup();
+    }
 });
